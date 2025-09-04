000d64126d610c36781abc9450baa614ee3e05ae
CasadoPedro <casado.pedro.martin@gmail.com>
2025-09-04 18:16:31 -0300

    Added an area indicator

--- js/utils.js
+++ js/utils.js
@@ -4,8 +4,12 @@ export function actualizarTurno(juego, turnoDiv) {
     const posicion = equipo.posicion || 0;
     turnoDiv.innerHTML = `
       Turno: ${equipo.nombre}<br>
-      Casillero: ${posicion}
+      Casillero: ${posicion} <br>
+      Zona: ${juego.tablero.zonas[juego.tablero.obtenerIndiceZona(posicion)]?.nombre}
     `;
+    // Cambiamos color de fondo dependiendo de la zona
+    const body = document.body;
+    body.style.backgroundColor = juego.tablero.zonas[juego.tablero.obtenerIndiceZona(posicion)]?.color;
     turnoDiv.style.backgroundColor = equipo.color || "#333333"; 
   }
   

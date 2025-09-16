bf32947f832788d8c7536c28113c8c665683eaa9
CasadoPedro <casado.pedro.martin@gmail.com>
2025-09-15 22:23:05 -0300

    Refactor page initialization and update turn handling logic

--- js/dialogo.js
+++ js/dialogo.js
@@ -1,30 +1,19 @@
-import Juego from "./juego.js";
-import { actualizarTurno } from "./utils.js";
+//import Juego from "./juego.js";
+import { iniciarPagina } from "./utils.js";
 
-let juegoData = JSON.parse(sessionStorage.getItem("juego"));
-if (!juegoData) window.location.href = "index.html";
-let juego = Juego.fromJSON(juegoData);
+let juego = iniciarPagina();
 
-const turnoDiv = document.querySelector(".turnoActual");
-actualizarTurno(juego, turnoDiv);
-
-const equipo = juego.equipos[juego.turnoActual];
-const posicion = equipo.posicion || 0;
-
-juego.tablero.obtenerPregunta
-const pregunta = juego.tablero.obtenerPregunta(posicion);
+// Obtener la pregunta según la posición del equipo actual
+const pregunta = juego.tablero.obtenerPregunta(juego.equipos[juego.turnoActual].posicion);
 
 // Mostrar la pregunta
 document.getElementById("pregunta-dialogo").innerText = pregunta ?? "No hay pregunta disponible para esta casilla.";
-
-console.log("Juego reconstruido:", juego);
-console.log("Turno actual:", juego.turnoActual, "Equipo:", equipo.nombre, "Posición:", posicion);
 
 const btnContinuar = document.getElementById("btn-continuar");
 
 btnContinuar.addEventListener("click", () => {
   // Pasar turno
-  juego.turnoActual = (juego.turnoActual + 1) % juego.equipos.length;
+  juego.siguienteTurno();
 
   // Guardar estado
   sessionStorage.setItem("juego", JSON.stringify(juego));
